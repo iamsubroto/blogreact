6bb756e6646ca3ac1577dfedb57f641ea3451343
iamsubroto <smartcoder500@gmail.com>
2018-12-21 09:01:09 -0800

    Updates

---
+++ precache-manifest.bfd3315c7d9a1fbab5fb6c1a8ea1c04f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19d082f34f04e8025541",
+    "url": "/blogreact/static/js/runtime~main.19d082f3.js"
+  },
+  {
+    "revision": "d33b829494e0b2e66052",
+    "url": "/blogreact/static/js/main.d33b8294.chunk.js"
+  },
+  {
+    "revision": "64b4c43f9ddbfcf74284",
+    "url": "/blogreact/static/js/1.64b4c43f.chunk.js"
+  },
+  {
+    "revision": "d33b829494e0b2e66052",
+    "url": "/blogreact/static/css/main.6345b4be.chunk.css"
+  },
+  {
+    "revision": "50f7a0f16e63d4f7172c39f5f19b5e8b",
+    "url": "/blogreact/index.html"
+  }
+];
